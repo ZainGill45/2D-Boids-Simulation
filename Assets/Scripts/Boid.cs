43e23d3b9a1f7d9b4f8c0dff511da85582c80182
Zain Gill <zaingill45@gmail.com>
2025-04-30 21:57:46 -0600

    Reset the project to just have one script instead of multiple

--- Assets/Scripts/Boid.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Boid : MonoBehaviour
-{
-    [Header("General Settings")]
-    [SerializeField] private float boidSpeed = 3f;
-    [SerializeField] private float rotationSpeed = 360f;
-}
